5242151fd02aae5d29c60cad026fb76027e54946
Dark869 <ing.victor97@gmail.com>
2026-03-15 14:40:49 -0600

    Modifield Entities to use UUID

--- src/main/java/com/dark869/auth_api/model/RefreshToken.java
+++ src/main/java/com/dark869/auth_api/model/RefreshToken.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "refresh_tokens")
@@ -15,8 +16,8 @@ import java.time.LocalDateTime;
 public class RefreshToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(name = "token_hash", nullable = false, unique = true)
     private String tokenHash;
